410bc1f7993f3f53f141cb5308318aee8f687ed5
Amit Jit Singh <singhamitjit@gmail.com>
2018-08-24 00:06:38 +0800

    fixes

--- 2_Machine_Learning/SVC/SVC_grid_searchCV.py
+++ 2_Machine_Learning/SVC/SVC_grid_searchCV.py
@@ -9,7 +9,7 @@ import pandas as pd
 #
 # dataset for grid search
 
-data = np.load('/home/amitjit/output/data_PM.npz')
+data = np.load('/home/amitjit/output/data_PM_gray.npz')
 X_train, y_train = data['X_train'], data['y_train']
 print('\nFinished importing data.')
 
@@ -20,11 +20,10 @@ print('\nFinished importing data.')
 # 10 is often helpful. Using a basis of 2, a finer
 # tuning can be achieved but at a much higher cost.
 
-C_range = np.logspace(-3, 4, 10)
-gamma_range = np.logspace(-9, 3, 10)
+C_range = np.logspace(-3, 4, 5)
+gamma_range = np.logspace(-9, 3, 5)
 param_grid = dict(gamma=gamma_range, C=C_range)
-cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2)
-grid = GridSearchCV(SVC(probability=True), param_grid=param_grid, cv=cv, refit=True )
+grid = GridSearchCV(SVC(probability=True), param_grid=param_grid, n_jobs=5, refit=True, return_train_score =True, verbose = 5)
 grid.fit(X_train, y_train)
 
 joblib.dump(grid,"/home/amitjit/output/SVC_gridCV.pkl")
