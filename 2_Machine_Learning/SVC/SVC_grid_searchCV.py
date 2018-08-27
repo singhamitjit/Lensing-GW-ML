a8ecae5280da3d2944ee165240c955e17100f22e
Amit Jit Singh <singhamitjit@gmail.com>
2018-08-27 16:02:31 +0800

    update

--- 2_Machine_Learning/SVC/SVC_grid_searchCV.py
+++ 2_Machine_Learning/SVC/SVC_grid_searchCV.py
@@ -24,7 +24,7 @@ C_range = np.logspace(-2, 4, 4)
 gamma_range = np.logspace(-9, 3, 10)
 param_grid = dict(gamma=gamma_range, C=C_range)
 cv = StratifiedShuffleSplit(n_splits=3, test_size=0.2)
-grid = GridSearchCV(SVC(), param_grid=param_grid, n_jobs=1, refit=True, cv=cv, return_train_score =True, verbose = 2)
+grid = GridSearchCV(SVC(), param_grid=param_grid, n_jobs=2, refit=True, cv=cv, return_train_score =True, verbose = 2)
 grid.fit(X_train, y_train)
 
 joblib.dump(grid,"/home/amitjit/output/SVC_gridCV.pkl")
